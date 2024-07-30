148821a6364dc3e0708db1fce8c38393ddfd8175
Edge Quintanilla <equintanilla@discoveryed.com>
2024-07-29 19:36:17 -0500

    TB-6174~tweet gen start

---
+++ tweet-generator.js
@@ -0,0 +1,9 @@
+import "dotenv/config";
+import { ChatOpenAI } from "@langchain/openai";
+import { PromptTemplate } from "@langchain/core/prompts"
+
+const OPEN_AI_KEY = process.env.OPENAI_PROJECT_KEY;
+
+PromptTemplate.
+
+console.log(OPEN_AI_KEY);
