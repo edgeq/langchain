2c81195bf7216bb1b23832e51ca314cceed638b1
Edge Quintanilla <equintanilla@discoveryed.com>
2024-08-09 14:02:44 -0500

    LANGCHAIN~comments for context

--- doc-retrieval-chain.js
+++ doc-retrieval-chain.js
@@ -9,7 +9,7 @@ import { createRetrievalChain } from "langchain/chains/retrieval";
 
 
 // {context} is what we will use to front-load our prompt
-// {input} is what we will use to invoke the retriever - nneds to be {input}
+// {input} is what we will use to invoke the retriever - needs to be {input}
 // Any other variables can be passed in, but will need to be declared at invocation
 const questionPrompt = ChatPromptTemplate.fromTemplate(
     `Answer the user's question from the following context.
@@ -25,33 +25,30 @@ const questionPrompt = ChatPromptTemplate.fromTemplate(
 // DOCUMENT CHAINS
 // are chains that can use docs as context to answer questions.
 // createStuffDocumentsChain is a helper function that
-// enables us to “stuff” all of the input documents
+// enables us to “stuff” all of the scraped documents
 // as {context} into the prompt when we invoke.
 const docsChain = await createStuffDocumentsChain({
     llm: openai,
     prompt: questionPrompt,
 })
-
+// SCRAPE FOR DOCUMENTS
 const webLoader = new CheerioWebBaseLoader(
     'https://en.wikipedia.org/wiki/Reverb_effect',
     {
         selector: '#bodyContent > h1, h2, p',
     }
 )
-
 const docs = await webLoader.load()
-
+// SPLIT DOCS FOR EMBEDDINGS
 const splitter = new RecursiveCharacterTextSplitter({
     chunkSize: 500,
     chunkOverlap: 30,
 })
-
 const splitDocs = await splitter.splitDocuments(docs)
-
+// EMBEDDINGS ASSIGN MATRIX VECTORS FOR MEANING
 const embeddings = new OpenAIEmbeddings({
     model: 'text-embedding-3-small'
 })
-
 const vectorStore = await MemoryVectorStore.fromDocuments(
     splitDocs,
     embeddings
@@ -63,10 +60,10 @@ const retriever = vectorStore.asRetriever()
 
 const retrievalChain = await createRetrievalChain({
     combineDocsChain: docsChain,
-    retriever,
+    retriever, // the context we want to "stuff"
 })
 const response = await retrievalChain.invoke({
-    input: 'explain echo chambers',
+    input: 'explain echo chambers', // ths input our prompt expects
 })
 
 console.log(response)
